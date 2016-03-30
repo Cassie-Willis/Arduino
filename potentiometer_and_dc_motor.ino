08c02feea4da211421a196c81595625912420f5b
Cassie Willis <ccw5ft@virginia.edu>
2016-03-30 14:35:43 -0400

    Added files via upload
    Working to have a DC motor with variable speed using a potentiomenter

---
+++ potentiometer_and_dc_motor.ino
@@ -0,0 +1,24 @@
+const int motorPin = 9;
+int sensorPin = 0;
+
+
+void setup() {
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int sensorValue;
+  sensorValue = analogRead(sensorPin);
+  
+  if (sensorValue <= 255) {
+    analogWrite(motorPin, sensorValue);
+  }
+  
+  else {
+    sensorValue = 100;
+    analogWrite(motorPin, sensorValue);
+  }
+
+}
